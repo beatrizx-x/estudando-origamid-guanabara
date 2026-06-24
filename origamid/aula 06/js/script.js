5a99c277079e949ca2984b54a46b979ba107d7b5
ANA BEATRIZ <oibibih@gmail.com>
2026-06-23 21:47:38 -0300

    objeto

--- origamid/aula 06/js/script.js
+++ origamid/aula 06/js/script.js
@@ -1,9 +1,21 @@
+// var btn = document.querySelector(".btn")
+// btn.addEventListener('click', function() {console.log("clicou")})
+// btn.classList
+
+// btn.classList.add("ativo");
+// btn.innerText;
+
+// btn.computedStyleMap()
+// console.log(btn.computedStyleMap())
+
+
+// nomeie 3 propriedades ou métodos de strings
+let nome = 'ana'
+nome.fixed
+nome.charAt
+nome.concat
+// nomeie 5 propriedades ou métodos de elementos do DOM
 var btn = document.querySelector(".btn")
-btn.addEventListener('click', function() {console.log("clicou")})
-btn.classList
 
-btn.classList.add("ativo");
-btn.innerText;
-
-btn.computedStyleMap()
-console.log(btn.computedStyleMap())
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
---
+++ origamid/aula 07/js/script.js
@@ -0,0 +1,60 @@
+// let videoGAmes = ["switch", "ps4", "xbox"];
+// videoGAmes.pop()//ele tira um da lista. tirou o xbox
+// let ultimoItem = videoGAmes.pop();//ele esta mostrando o ultimo q ele tirou, xbox.
+// videoGAmes.push("biaplay");//adiciona na lista
+
+// for (let numero = 0; numero < 10; numero++) {
+//   console.log(numero);
+// }
+
+// let i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   i = i + 2;
+// }
+
+// let videoGAmes = ["switch", "ps4", "xbox", "biaplay"];
+// for(let item = 0; item < videoGAmes.length; item++){
+// console.log(videoGAmes[item]);
+// if(videoGAmes[item] === "ps4"){
+//   console.log("ps4 é legal")
+//   break;
+// }
+// }//leght mostra quantos itens tem na array
+
+
+// let frutas = ["banana", "maçã", "abacaxi"]
+// frutas.forEach(function(fruta){
+// console.log(fruta)})
+
+
+// let claEscolhido = {
+//   mago: {
+//     poder: 'fogo',
+//     itensDeCla: "cajado de fogo"
+//   },
+//   guerreiro: {
+//     poder: 'força',
+//     itensDeCla: "armaduras e espadas"
+//   },
+//   elfo: {
+//     poder: 'agilidade',
+//     itensDeCla: 'arco e flecha'
+//   },
+//   ferreiro: {
+//     poder: 'construção',
+//     itensDeCla: 'martelo e bigorna'
+//   }
+// };
+
+// let nomeJogador = prompt("Digite seu nome de jogador:");
+// let respostaCla = prompt("Escolha seu clã: mago, elfo, ferreiro, guerreiro").toLowerCase();
+
+// let pessoa1 = {
+//   nome: nomeJogador,
+//   cla: respostaCla,
+//   poder: claEscolhido[respostaCla].poder,
+//   itens: claEscolhido[respostaCla].itensDeCla
+// };
+
+// alert("Personagem criado! Bem vindo(a), " + pessoa1.nome + "!");
